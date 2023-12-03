b029b152dfdc3487ac99d90b99805da54a7c33e8
seregaseregovich <sergeisergeevich@mail.ru>
2023-12-03 20:58:46 +0300

    New file

--- Списки. Транспонирование матрицы.py
+++
@@ -1,8 +0,0 @@
-a = [[0, 10, 20, 30], [40, 50, 60, 70], [80, 90, 100, 110]]
-for i in a:
-    print(i)
-print()
-y = [[row[i] for row in a] for i in range(len(a[0]))]
-for i in y:
-    print(i)
-    
