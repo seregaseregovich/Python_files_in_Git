7b436f4e1d560eadaa44d7607f8497ca03b7368e
seregaseregovich <sergeisergeevich@mail.ru>
2023-12-09 18:08:04 +0300

    Training

--- 11.py
+++ 11.py
@@ -1,3 +1,10 @@
-z = [f'{x} x {y} = {x * y}' for x in range(1, 10) for y in range(1, 10)]
-for i in z:
-    print(i)
+a = [[1, 2, 3, 4], [11, 22, 33, 44], [111, 222, 333, 444]]
+b = [[10, 20, 30, 40], [100, 200, 300, 400], [1000, 2000, 3000, 4000]]
+c = []
+for i, row in enumerate(a):
+    r = []
+    for j, x in enumerate(row):
+        r.append(x + b[i][j])
+    c.append(r)
+
+print(c)
