20eba440475f9c88c6a4fac08c68e3ba512d680f
seregaseregovich <sergeisergeevich@mail.ru>
2023-12-09 17:58:37 +0300

    Training

--- 11.py
+++ 11.py
@@ -1,5 +1,3 @@
-l1 = [[2, 4, 3], [1, 5, 6], [9], [7, 9, 0]]
-l2 = [x for row in l1 for x in row]
-print(l1, l2, sep='\n')
-l3 = l2[::-1]
-print(l3 == l2[::-1])
+z = [f'{x} x {y} = {x * y}' for x in range(1, 10) for y in range(1, 10)]
+for i in z:
+    print(i)
