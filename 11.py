d1291fc1eb91a28d1d06173b5b53d9642a5d8678
seregaseregovich <sergeisergeevich@mail.ru>
2023-12-09 21:51:50 +0300

    training

--- 11.py
+++ 11.py
@@ -1,10 +1,1 @@
-a = [[1, 2, 3, 4], [11, 22, 33, 44], [111, 222, 333, 444]]
-b = [[10, 20, 30, 40], [100, 200, 300, 400], [1000, 2000, 3000, 4000]]
-c = []
-for i, row in enumerate(a):
-    r = []
-    for j, x in enumerate(row):
-        r.append(x + b[i][j])
-    c.append(r)
-
-print(c)
+print(sum([int(x) for x in input()]))
