a380ebbd9219f49ea04983558b90e063607f8db6
seregaseregovich <sergeisergeevich@mail.ru>
2023-12-13 21:29:14 +0300

    Added solved task

--- 11.py
+++ 11.py
@@ -1,4 +1,7 @@
-try:
-    print(sum([int(x) for x in input()]))
-except ValueError:
-    print('Failed')
+lst = ['a', 'b']
+lst2 = []
+for i in range(1, 6):
+    lst2.append(lst[0] + str(i))
+    lst2.append(lst[1] + str(i))
+print(lst2)
+
