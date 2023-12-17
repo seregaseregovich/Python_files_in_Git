251dde3816b94fac3c1cc7ffc7f10765a3d8f45c
seregaseregovich <sergeisergeevich@mail.ru>
2023-12-17 17:47:21 +0300

    Added some changes

--- Списки. Создание a1 a2 a3 b1 b2 b3  из а и b.py
+++ Списки. Создание a1 a2 a3 b1 b2 b3  из а и b.py
@@ -1,8 +1,16 @@
 # Программа для создания имен переменных:
-
+# ВАРИАНТ 1:
 l1 = ['a', 'b']
 n = 4
-l2 = ['{}{}'.format(x, y) for y in range(1, n+1) for x in l1]
+l2 = [f'{x}{y}' for y in range(1, n + 1) for x in l1]
 print(l2)
 # Результат:
 # ['a1', 'b1', 'a2', 'b2', 'a3', 'b3', 'a4', 'b4']
+
+# ВАРИАНТ 2:
+lst = ['a', 'b']
+lst2 = []
+for i in range(1, 5):
+    lst2.append(lst[0] + str(i))
+    lst2.append(lst[1] + str(i))
+print(lst2)
