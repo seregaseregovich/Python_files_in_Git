a9232b41bea3d6b56c6baaef88f6f02741869fc9
seregaseregovich <sergeisergeevich@mail.ru>
2023-12-19 20:34:01 +0300

    Task

--- 11.py
+++ 11.py
@@ -1,8 +1,8 @@
 import operator
 
 dict1 = {
-    'Sergei': {'date': 3, 'month': 'september', 'year': 1975},
-    'Helen': {'date': 6, 'month': 'february', 'year': 1973}
+    'Sergei': {'date': 12, 'month': 'may', 'year': 1975},
+    'Helen': {'date': 16, 'month': 'june', 'year': 1976}
 }
 for i in dict1:
     if dict1[i]['year'] > 1971:
