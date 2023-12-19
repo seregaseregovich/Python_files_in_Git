eed1454f1d182c03cae27adf06777cce943fef87
seregaseregovich <sergeisergeevich@mail.ru>
2023-12-19 20:41:39 +0300

    Task. Import module

--- 11.py
+++ 11.py
@@ -1,4 +1,5 @@
 import operator
+import Функция_Обратный_отсчет
 
 dict1 = {
     'Sergei': {'date': 12, 'month': 'may', 'year': 1975},
@@ -10,3 +11,6 @@ for i in dict1:
 
 sorted_dict1 = sorted(dict1.items(), key=operator.itemgetter(0), reverse=False)
 print(dict1, dict(sorted_dict1), sep='\n')
+x = int(input("Enter the integer number: "))
+for i in Функция_Обратный_отсчет.countdown(x):
+    print(i)
