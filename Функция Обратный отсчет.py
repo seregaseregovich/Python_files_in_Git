6b6fe292598956e31986020291c3aa348304f7f6
seregaseregovich <sergeisergeevich@mail.ru>
2023-12-19 20:37:15 +0300

    Formatted

--- Функция Обратный отсчет.py
+++ Функция Обратный отсчет.py
@@ -2,7 +2,9 @@ def countdown(n):
     print("Обратный отсчет")
     while n > 0:
         yield n
-        n -= 1 # если сделать "+",
+        n -= 1  # если сделать "+",
+
+
 # то будет отсчитывать от 500 в сторону увеличения
 x = int(input("Enter the integer number: "))
 for i in countdown(x):
