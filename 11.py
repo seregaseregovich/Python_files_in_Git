e5a471e6a7f52778d03066e80ba5c9beaf4d7af3
seregaseregovich <sergeisergeevich@mail.ru>
2023-12-19 20:21:42 +0300

    Task

--- 11.py
+++ 11.py
@@ -1,9 +1,8 @@
 dict1 = {
-    'на кол': 1,
-    'неудовлетворительно': '2',
-    'удовлетворительно': '3',
-    'хорошо': '4',
-    'отлично': '5',
+    'Sergei': {'date': 3, 'month': 'september', 'year': 1971},
+    'Helen': {'date': 6, 'month': 'february', 'year': 1973}
 }
-dict2 = {key.upper(): int(value) for key, value in dict1.items()}
-print(dict2)
+for i in dict1:
+    if dict1[i]['year'] > 1971:
+        print(f'{i}, date of birth {dict1[i]['date']} {dict1[i]['month']} {dict1[i]['year']}')
+
