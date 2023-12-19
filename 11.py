1becc764269b9f181388c85c276ed18d6fc5bc75
seregaseregovich <sergeisergeevich@mail.ru>
2023-12-19 21:08:18 +0300

    A few changes

--- 11.py
+++ 11.py
@@ -1,16 +1,3 @@
-import operator
-import Функция_Обратный_отсчет
-
-dict1 = {
-    'Sergei': {'date': 12, 'month': 'may', 'year': 1975, 'sex': 'male'},
-    'Helen': {'date': 16, 'month': 'june', 'year': 1976, 'sex': 'female'}
-}
-for i in dict1:
-    if dict1[i]['year'] > 1971:
-        print(f'{i}, date of birth {dict1[i]['date']} {dict1[i]['month']} {dict1[i]['year']}')
-
-sorted_dict1 = sorted(dict1.items(), key=operator.itemgetter(0), reverse=False)
-print(dict1, dict(sorted_dict1), sep='\n')
-x = int(input("Enter the integer number: "))
-for i in Функция_Обратный_отсчет.countdown(x):
-    print(i)
+l1, l2 = [1, 3, 5, 7, 9, 10], [2, 4, 6, 8]
+l1[-1:] = l2
+print(l1)
