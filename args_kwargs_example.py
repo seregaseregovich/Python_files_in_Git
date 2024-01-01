757d1ebafcd2f72686b706ef749a31322a64bd01
seregaseregovich <sergeisergeevich@mail.ru>
2024-01-01 20:05:14 +0300

    Added a few changes

--- args_kwargs_example.py
+++ args_kwargs_example.py
@@ -1,8 +1,37 @@
-import time
+'''Data packing and unpacking:
+*, **, args, kwargs'''
 
-start = time.time()
+
+# An example of data packing:
+# =====================================
+
+a, *b, c = 'Hello'
+print(a, b, c)  # res = H ['e', 'l', 'l'] o
+a, b, *c = 1, 2, 3, 4, 5, 6, 7
+print(a, b, c)  # res = 1 2 [3, 4, 5, 6, 7]
+a, *_, c = 'Hello'
+print(a, c)  # res = H o
+a, b, *_ = 1, 2, 3, 4, 5, 6, 7
+print(a, b)  # res = 1 2
+
+# An example of data unpacking:
+# =====================================
+
+lst = [1, 2, 3, 4, 5]
+a, *b = lst
+print(a, b)  # res = 1 [2, 3, 4, 5]
+*a, b = lst
+print(a, b)  # res = [1, 2, 3, 4] 5
+a, *b, c = lst
+print(a, b, c)  # res = 1 [2, 3, 4] 5
+a, *_, c = lst
+print(a, c)  # res = 1 5
+a, b, *_ = lst
+print(a, b)  # res = 1 2
 
 
+# An example of using args and kwargs in the function:
+# ====================================================
 def os_path(disc, *args, sep='\\', **kwargs):
     args = (disc,) + args
     if 'trim' in kwargs and kwargs['trim']:
@@ -15,7 +44,3 @@ p = os_path('f:', ' stepic.org ',
             ' Kind,_kind python ',
             '37\\39._Functions.docs', sep='/', trim=True)
 print(p)
-
-stop = time.time()
-dt = stop - start
-print(dt)
