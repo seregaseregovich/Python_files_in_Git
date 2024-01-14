b344c89b9a3a0178668a87abc9d7b6e270d18e3b
seregaseregovich <sergeisergeevich@mail.ru>
2024-01-14 21:52:39 +0300

    A few changes

--- Декораторы - скорость вып. функции.py
+++ Декораторы - скорость вып. функции.py
@@ -1,3 +1,6 @@
+'''Декоратор test_time предназначен для проверки
+или сравнения скорости выполнения функций'''
+
 import time
 
 
@@ -14,37 +17,16 @@ def test_time(func):
 
 
 @test_time
-def to_binary1(n):
-    for i in range(100000):
-        continue
-    return "{:0b}".format(n & 0xffffffff)
+def func1(n):
+    return n
 
 
 @test_time
-def to_binary2(n):
-    for i in range(100000):
-        continue
-    if n > 0:
-        a = bin(int(n))
-        return a[2:]
-    elif n == 0:
-        return '0'
-    else:
-        a = str(int(bin(2**32)[2:]) - int(bin(abs(n))[2:]))
-        b = ''.join('1' if i == '9' else '0' for i in a)
-        return b
+def func2(n):
+    return n
 
 
-# get_nod = test_time(get_nod) - вместо этой строки
-# ставим декоратор @test_time перед этой функцией
-res1 = to_binary1(3)
+res1 = func1(3)
+res2 = func2(3)
 
-
-# get_nod2 = test_time(get_nod2) - вместо этой строки
-# # ставим декоратор @test_time перед этой функцией
-res2 = to_binary2(3)
-
-print(res1, res2, sep='\n')
-
-
-# https://www.youtube.com/watch?v=v0qZPplzwUQ&list=PLA0M1Bcd0w8yWHh2V70bTtbVxJICrnJHd&index=47
+print(f'res_func1 = {res1}', f'res_func2 = {res2}', sep='\n')
