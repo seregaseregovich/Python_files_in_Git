9a4ba6cc622ca7786f31b6868ca10566201b92f6
seregaseregovich <sergeisergeevich@mail.ru>
2024-02-11 20:44:18 +0300

    Added a few changes

--- Файлы. Открытие_чтение_запись.py
+++ Файлы. Открытие_чтение_запись.py
@@ -4,7 +4,7 @@
 # ВАРИАНТ 1:
 try:  # Проверяет наличие ошибок в работе с файлом.
     try:  # Проверяет наличие ошибок в пути или наличии файла.
-        with open('abb.txt', 'r') as file:
+        with open('abb.txt', 'r', encoding='utf-8') as file:
             print(file.readline())  # Вывод одной строки: Hello, hello, hello.
     except FileNotFoundError:
         print("Check filename or path to the file!")
@@ -14,11 +14,14 @@ except:
 
 # ВАРИАНТ 2 (упрощенный):
 try:
-    with open('abb.txt', 'r') as file:
-        print(file.readlines())
+    with open('D:/DATA/Работа/IT/Python_files_in_Git'
+              '/abb.txt', 'r', encoding='utf-8') as file:
+        print(file.readline(4))
+        print(file.readline())
+        file.seek()  # переводит точку считывания в нужную позицию
+        print(file.tell())  # возвращает текущую позицию точки считывания текста файла
+        print(file.readline(10))
         # ['Hello, hello, hello.\n', 'Hello, hello, hello.\n']
         # При наличии нескольких строк выводится в виде списка.
 except FileNotFoundError:
     print("Check filename or path to the file!")
-
-#
