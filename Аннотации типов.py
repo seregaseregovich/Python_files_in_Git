9d5dddf5bb2f84f28e86d302c0395904124736c4
seregaseregovich <sergeisergeevich@mail.ru>
2024-02-25 17:16:54 +0300

    a few changes have been added

--- Аннотации типов.py
+++ Аннотации типов.py
@@ -24,11 +24,20 @@ print(mult1(2), mult1.__annotations__)
 # (с использованием символа объединения "|"):
 
 
-def mult2(x: int | float, y: int | float = 44) -> int | float | str:
+def mult2_1(x: int | float, y: int | float = 44) -> int | float | str:
     return str(x * y)
 
 
-print(mult2(2), mult2.__annotations__)
+# ... или можно назначить типам данных свою переменную:
+type_1 = int | float
+type_2 = int | float | str
+
+
+def mult2_2(x: type_1, y: type_1 = 44) -> type_2:
+    return str(x * y)
+
+
+print(mult2_1(2), mult2_1.__annotations__)
 # 88 {'x': int | float, 'y': int | float, 'return': int | float | str}
 
 
@@ -41,10 +50,19 @@ print(mult2(2), mult2.__annotations__)
 from typing import Union, Optional, Any, Final
 
 
-def mult3(x: Union[int, float], y: Union[int, float] = 444) -> Union[int, float, str]:
+def mult3_1(x: Union[int, float], y: Union[int, float] = 444) -> Union[int, float, str]:
     return str(x * y)
 
 
-print(mult3(2), mult3.__annotations__)
+# ... или можно назначить типам данных свою переменную:
+type_1 = Union[int, float]
+type_2 = Union[int, float, str]
+
+
+def mult3_2(x: type_1, y: type_1 = 44) -> type_2:
+    return str(x * y)
+
+
+print(mult3_1(2), mult3_1.__annotations__)
 # 888 {'x': typing.Union[int, float], 'y': typing.Union[int, float],
 # 'return': typing.Union[int, float, str]}
